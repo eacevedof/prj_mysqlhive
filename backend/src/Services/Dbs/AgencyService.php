0954a668bb44c7e273aaf5bc79b36f4fe103d629
eacevedo <eacevedo@ip-192-168-23-1.eu-west-1.compute.internal>
2019-01-15 16:29:51 +0100

    Nuevo AgencyService.php

---
+++ backend/src/Services/Dbs/AgencyService.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name App\Services\Dbs\AgencyService 
+ * @file AgencyService.php 1.0.0
+ * @date 15-01-2018 19:00 SPAIN
+ * @observations
+ */
+namespace App\Services\Dbs;
+
+use App\Services\AppService;
+
+class AgencyService extends AppService
+{
+
+}//AgencyService
