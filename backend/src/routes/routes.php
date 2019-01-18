ec02d08cb1b50c959272f4b93c55adc52bfee19a
Eduardo TC <eacevedof@yahoo.es>
2019-01-18 11:07:00 +0100

    controlador para mostrar logs

--- backend/src/routes/routes.php
+++ backend/src/routes/routes.php
@@ -3,17 +3,12 @@
 //mapeo de rutas y controladores
 
 return [   
-/*  
-Listado de employees con el departamento, cargo y salario actuales. 
-Ordenado por fecha de contratación y limitado a 50 
-*/  
     ["url"=>"/","controller"=>"App\Controllers\NotFoundController","method"=>"index"],
     ["url"=>"/agency","controller"=>"App\Controllers\Dbs\AgencyController","method"=>"index"],
     ["url"=>"/agency/","controller"=>"App\Controllers\Dbs\AgencyController","method"=>"index"],
     ["url"=>"/draco","controller"=>"App\Controllers\Dbs\DracoController","method"=>"index"],
     ["url"=>"/draco/","controller"=>"App\Controllers\Dbs\DracoController","method"=>"index"],
     ["url"=>"/logs","controller"=>"App\Controllers\LogsController","method"=>"index"],
-
 
 //resto de rutas    
     ["url"=>"/404","controller"=>"App\Controllers\NotFoundController","method"=>"error_404"]
