dcf22614520a6d8ce533e1dee63f467aab1e8117
Eduardo TC <eacevedof@yahoo.es>
2019-01-29 10:56:55 +0100

    script de creacion de vista

--- backend/src/Behaviours/templates/agency/tpl_dw.ddl.sql
+++ backend/src/Behaviours/templates/agency/tpl_dw.ddl.sql
@@ -12,5 +12,9 @@ CLUSTERED BY(%fieldnamepk%) INTO 5 BUCKETS
 STORED AS ORC
 TBLPROPERTIES("orc.compress"="SNAPPY",'transactional'='true');
 
+-- las vistas solo se crean para tablas transaccionales (di_<tabla>) para las ft no hacen falta
 INSERT INTO TABLE dw.%tablenameprefix%  VALUES (%fieldsvalue%);
 
+CREATE VIEW dw.%tablenameprefix%_view AS
+SELECT * FROM dw.%tablenameprefix%;
+ 
