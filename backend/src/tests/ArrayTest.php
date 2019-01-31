49f34d255dbf0d7adbe3a7ec1e30e6511a956807
Eduardo TC <eacevedof@yahoo.es>
2019-01-31 09:34:44 +0100

    shuffle 2 times

--- backend/src/tests/ArrayTest.php
+++ backend/src/tests/ArrayTest.php
@@ -19,14 +19,20 @@ class ArrayTest extends TestCase
         $arOrig = [
             "r1" => ["field1"=>"val-11","field2"=>"val-12","field3"=>"val-13"],
             "r2" => ["field2"=>"val-21","field3"=>"val-22","field4"=>"val-23"],
-            "r3" => ["field2"=>"val-31","field3"=>"val-32","field4"=>"val-33"]
+            "r3" => ["field2"=>"val-31","field3"=>"val-32","field4"=>"val-33"],
+            "r4" => ["field2"=>"val-41","field3"=>"val-42","field4"=>"val-43"],
+            "r5" => ["field2"=>"val-51","field3"=>"val-52","field4"=>"val-53"]
         ];
         $iCountO = count($arOrig);
         $this->log($arOrig,"arOrig $iCountO");
         
         shuffle($arOrig);
         $iCountSh = count($arOrig);
-        $this->log($arOrig,"shuffled $iCountSh");
+        $this->log($arOrig,"shuffled1 $iCountSh");
+        
+        shuffle($arOrig);
+        $iCountSh = count($arOrig);
+        $this->log($arOrig,"shuffled2 $iCountSh");     
         
         $this->assertEquals(TRUE,(is_array($arOrig) && ($iCountO === $iCountSh)));
     }
