db678a536b755137e46a1b56b7e7d10d3844bd33
Eduardo TC <eacevedof@yahoo.es>
2019-02-25 18:20:19 +0100

    ajustar combinatoria

--- backend/src/Services/Dbs/AgregacionService.php
+++ backend/src/Services/Dbs/AgregacionService.php
@@ -17,7 +17,7 @@ class AgregacionService extends AppService
 {
     private $oModel;
     private $oRnd;
-    
+       
     public function __construct() 
     {
         parent::__construct();
@@ -25,15 +25,18 @@ class AgregacionService extends AppService
         $this->oRnd = new RamdomizerService();
     }
     
+    private function pr($sString){print_r($sString."\n");}    
+    
     private function truncate_operations()
     {
+        
         $sSQL = "TRUNCATE TABLE tbl_operation";
         $this->oModel->execute($sSQL);
     }
     
     public function add_operation()
     {
-        print_r("AgregacionService.add_operation");
+        $this->pr("AgregacionService.add_operation");
         $this->oModel->set_table("tbl_operation");
         
         $arData["op_d1"] = "d1".$this->oRnd->get_item(["A","B","C"]);
@@ -41,17 +44,22 @@ class AgregacionService extends AppService
         $arData["op_d3"] = "d3".$this->oRnd->get_item(["A","B","C"]);
         $arData["op_d4"] = "d4".$this->oRnd->get_item(["A","B","C"]);
         $arData["op_d5"] = "d5".$this->oRnd->get_item(["A","B","C"]);
+        
+        $arData["op_atr1"] = "a1-".$this->oRnd->get_substring_len(10);
+        $arData["op_atr2"] = "a2-".$this->oRnd->get_substring_len(15);
+        $arData["op_atr3"] = "a3-".$this->oRnd->get_substring_len(20);
 
         $arData["op_m1"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
         $arData["op_m2"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
         $arData["op_m3"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
-        $arData["op_m4"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));        
+        $arData["op_m4"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
+        
         $this->oModel->insert($arData,0);
     }
     
     public function add_operations($iNum=1)
     {
-        print_r("AgregacionService.add_operations($iNum)");
+        $this->pr("AgregacionService.add_operations($iNum)");
         for($i=0;$i<$iNum;$i++)
         {
             $this->add_operation();
@@ -60,16 +68,20 @@ class AgregacionService extends AppService
     
     public function modif_operation()
     {
-        print_r("AgregacionService.add_operation");
+        $this->pr("AgregacionService.modif_operation");
         $this->oModel->set_table("tbl_operation");
         $this->oModel->set_pk("id");
         
         $sSQL = "SELECT id FROM tbl_operation";
         $arIds = $this->oModel->query($sSQL,0);
         $arIds = array_column($arIds,"id");
-        //print_r($arIds);die;
+        //$this->pr($arIds);die;
         
         $arData["id"] = $this->oRnd->get_item($arIds);
+        
+        $arData["op_atr1"] = "a1-".$this->oRnd->get_substring_len(10);
+        $arData["op_atr2"] = "a2-".$this->oRnd->get_substring_len(15);
+        $arData["op_atr3"] = "a3-".$this->oRnd->get_substring_len(20);
         $arData["op_m1"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
         $arData["op_m2"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
         $arData["op_m3"] = $this->oRnd->get_int(1,$this->oRnd->get_int(0,1000));
@@ -82,7 +94,7 @@ class AgregacionService extends AppService
     {
         for($i=0;$i<$iNum;$i++)
         {
-            print_r("AgregacionService.modif_operations $i \n");
+            $this->pr("AgregacionService.modif_operations $i");
             $this->modif_operation();
         }
     }
@@ -118,8 +130,33 @@ class AgregacionService extends AppService
     
     public function run()
     {
-        print_r("AgregacionService.run");
+        $iMins = 60;
+        $this->pr("AgregacionService.run");
+        $this->pr("
+        -comprobar nuevos de hace $iMins mins
+        -   si hay nuevos
+                - se obtienen todas las combinaciones de dimensiones que forman las claves
+                - 
+                
+        ");
+
+    }
+    
+    private function get_combinatoria($n,$k)
+    {
+        //r = (n!/k!)*(1/(n-k)!)
+        //$r = ()
+    }
+
+
+
+
+    public function get_posibilites()
+    {
+        $arElemnts = ["a","b","c","d"];
+        
     }
         
+    
 }//AgregacionService
 
