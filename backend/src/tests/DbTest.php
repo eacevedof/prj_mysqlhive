2cac8e990a082123b6879b5a46047301eb5f5a1c
Eduardo TC <eacevedof@yahoo.es>
2019-03-12 17:12:28 +0100

    no me pasa el test de bulk insert

--- backend/src/tests/DbTest.php
+++ backend/src/tests/DbTest.php
@@ -3,6 +3,7 @@
 // ./vendor/bin/phpunit --bootstrap ./vendor/theframework/bootstrap.php ./src/tests/DbTest.php --color=auto
 // ./vendor/bin/phpunit --bootstrap ./vendor/theframework/bootstrap.php ./src/tests
 use PHPUnit\Framework\TestCase;
+use TheFramework\Components\ComponentFile;
 use TheFramework\Components\ComponentLog;
 use TheFramework\Components\Db\ComponentMysql;
 
@@ -21,6 +22,46 @@ class DbTest extends TestCase
         $isFile = is_file($sFile);
         $this->assertEquals(TRUE,$isFile);
     }
+    
+    public function test_bulk_insert()
+    {
+        $sFile = __DIR__."/../config/config.php";
+        $arConfig = include($sFile);      
+        $arConfig = $arConfig["db"];
+        $oDb = new ComponentMysql($arConfig);
+        
+        $file = __DIR__.DIRECTORY_SEPARATOR."logs";
+        $db_table_from = "tbl_operation";
+        $db_table_dest = "tbl_operation_bulk";
+        
+        $oLog = new ComponentFile(__DIR__.DIRECTORY_SEPARATOR."logs",$db_table_from);
+        
+        $sSQL = "
+        SELECT * 
+        FROM db_agregacion.tbl_operation 
+        WHERE 1=1'
+        ";
+        
+        $arRows = $oDb->query($sSQL);
+        foreach($arRows as $i=>$arRow)
+        {
+            $fieldvalues = preg_replace("[\n|\r|\n\r]","",implode("\001",$arRow));
+            $iSaved = $oLog->save($fieldvalues); 
+            $this->log($arRow,"ARROW $i");
+            $this->log($fieldvalues,"STRING $i");
+        }
+
+        $sql="
+        load data local infile '$file'
+        replace into table $db_table_dest
+        CHARACTER SET utf8
+        fields terminated by '\001'
+        lines terminated by '\n'";        
+        $oDb->exec($sql);
+        
+        $this->assertEquals(FALSE,$oDb->is_error());
+    }
+    
     
     /**
      *  @depends test_exists_config_file
