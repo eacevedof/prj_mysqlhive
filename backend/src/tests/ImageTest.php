2b3130dcb010105e7c242b969a4a72ef6032f14d
Eduardo TC <eacevedof@yahoo.es>
2019-10-07 16:20:50 +0200

    prueba exif_read_data

---
+++ backend/src/tests/ImageTest.php
@@ -0,0 +1,26 @@
+<?php
+// en: /<project>/backend 
+// ./vendor/bin/phpunit --bootstrap ./vendor/theframework/bootstrap.php ./src/tests/ImageTest.php --color=auto
+// ./vendor/bin/phpunit --bootstrap ./vendor/theframework/bootstrap.php ./src/tests
+use PHPUnit\Framework\TestCase;
+use TheFramework\Components\ComponentLog;
+
+class ImageTest extends TestCase
+{    
+    private function log($mxVar,$sTitle=NULL)
+    {
+        $oLog = new ComponentLog("logs",__DIR__);
+        $oLog->save($mxVar,$sTitle);
+        if($sTitle) print_r("\n$sTitle\n");
+        print_r("\n$mxVar\n");
+    }
+    
+    public function test_metadata()
+    {
+        $url = "https://d3mfch55dy5n4c.cloudfront.net/contenidos-noticias/news_744_el-tackling-mobile-ad-fraud-se-celebra-en-telecoming-_20191007010611.jpg";
+        $exif = exif_read_data($url,"IFD0");
+        print_r($exif);
+        $this->assertEquals(TRUE,is_array($exif));
+    }  
+    
+}//ImageTest
